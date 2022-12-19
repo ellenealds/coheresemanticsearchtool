d3490bf2816c0544e3554dac9c07a5aa17dc4733
ellenealds <elle.neal@bpaquality.co.uk>
2022-12-19 06:30:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -34,35 +34,6 @@ def load_data(df,):
 
 df, search_index = load_data(df)
 
-
-# add a title to the app
-st.title('Cohere Doc Semantic Search Tool')
-
-# add a search bar
-search_bar = st.text_input('Search for a document')
-
-# add a select box for the search type
-
-search_type = st.selectbox('Search Type', ('Exact', 'Fuzzy'))
-
-# add a slider for the number of results to return
-
-num_results = st.slider('Number of Results', 1, 10, 5)
-
-# add a button to trigger the search
-
-if st.button('Search'):
-    # do the search
-    if search_type == 'Exact':
-        results = df[df['text'].str.contains(search_bar)]
-    else:
-        results = df[df['text'].str.contains(search_bar, case=False)]
-    # display the results
-    st.write(results.head(num_results))
-
-
-
-    
 def search(query, n_results, df, search_index, co):
     # Get the query's embedding
     query_embed = co.embed(texts=[query],
@@ -74,9 +45,25 @@ def search(query, n_results, df, search_index, co):
     # Return the results
     return df.iloc[neighbors]
 
-# Search for the query
-results = search(search_bar, num_results, df, search_index, co)
+# add a title to the app
+st.title('Cohere Doc Semantic Search Tool')
 
-# Display the results in a bootstrap card
-for i, row in results.iterrows():
-    st.write(row['text'])
+# add a search bar
+search_bar = st.text_input('Search for a document')
+
+# add a select box for the search type
+
+search_type = st.selectbox('Search Type', ('Exact', 'Fuzzy', 'Similar'))
+
+# add a slider for the number of results to return
+
+num_results = st.slider('Number of Results', 1, 10, 5)
+
+# add a button to trigger the search
+
+if st.button('Search'):
+    # do the search
+    results = search(search_bar, num_results, df, search_index, co)
+    # display the results
+    for i, row in results.iterrows():
+        st.write(row['text'])
